cd370e8db769ac17a0f5f37b43c823903903b211
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2024-11-17 12:46:24 +0100

    Probando la conexión con las funciones del módulo, tras crear un objeto

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,8 +1,10 @@
 import * as miModulo from "./gestionPresupuesto.js"
-
+let coche = new miModulo.CrearGasto("Coche",4,"2021/02/01","ITV")
 function mostrarDatoEnId(){
-    let coche = new miModulo.CrearGasto("Coche",4,"2021/02/01","ITV")
-    alert(coche.descripcion)
+    const idElemento = document.querySelector("#idElemento");
+    const valor = document.querySelector("#valor");
+    idElemento.innerHTML = miModulo.calcularBalance();
+    valor.innerHTML =miModulo.mostrarPresupuesto();
 }
 
 function mostrarGastoWeb(){
@@ -12,17 +14,25 @@ function mostrarGastoWeb(){
 function mostrarGastosAgrupadosWeb(){
     return
 }
-const boton = document.querySelector("#boton");/*
-boton.addEventListener("click",function(){
-    alert("Hola");
-})*/
-boton.addEventListener("click",mostrarDatoEnId);
 
+
+const boton = document.querySelector("#boton");
+boton.addEventListener("click",function(){
+
+    mostrarDatoEnId();
+});
 /*
+const boton = document.querySelector("#boton");
+boton.addEventListener("click",function(){
+let coche = new miModulo.CrearGasto("Coche",4,"2021/02/01","ITV")
+    alert(coche.descripcion)
+});
+*/
+
 export {
     
     mostrarDatoEnId,
     mostrarGastoWeb,
     mostrarGastosAgrupadosWeb
 
-}*/
+}
