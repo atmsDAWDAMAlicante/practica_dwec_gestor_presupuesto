26afc93879068200024dc2234159feca43f67fab
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-01-25 18:16:28 +0100

    Modificación: className a classList.add en mostrarGastoWeb

--- js/gestionPresupuestoWeb.js
+++ js/gestionPresupuestoWeb.js
@@ -1,43 +1,65 @@
 import * as miModulo from "./gestionPresupuesto.js"
 let coche = new miModulo.CrearGasto("Coche",4,"2021/02/01","ITV")
 
-const idElemento = document.querySelector("#idElemento");
-const valor = document.querySelector("#valor");
+//const idElemento = document.querySelector("#idElemento");
+//const valor = document.querySelector("#valor");
 
-function mostrarDatoEnId(valorIdElemento, valorEnValor){
-    // Primero pongo las dos constantes como globales
-    idElemento.innerHTML = valorIdElemento;
-    valor.innerHTML = valorEnValor;
+function mostrarDatoEnId(idElemento, valor){
+    
+    let elemento = document.getElementById(idElemento);
+    elemento.innerHTML = valor;
 }
 
 function mostrarGastoWeb(idElemento, gasto){
 
+    let contenedor = document.getElementById(idElemento);
+
     // El div envolvente
     const divGranGasto = document.createElement("div");
-    divGranGasto.className = "gasto";
+    //divGranGasto.className = "gasto";
+    divGranGasto.classList.add("gasto");
 
     // El div de la descripción
     const divDescripcion = document.createElement("div");
-    divDescripcion.className = "gasto-descripcion";
+    //divDescripcion.className = "gasto-descripcion";
+    divDescripcion.classList.add("gasto-descripcion");
     divDescripcion.textContent = gasto.descripcion;
 
     // El div de la fecha
     const divFecha = document.createElement("div");
-    divFecha.className = "gasto-fecha";
+    //divFecha.className = "gasto-fecha";
+    divFecha.classList.add("gasto-fecha");
     divFecha.textContent = gasto.fecha;
 
+     // El div del valor -- este lo había olvidado
+     const divValor = document.createElement("div");
+     //divFecha.className = "gasto-valor";
+     divValor.classList.add("gasto-valor");
+     divValor.textContent = gasto.valor;
+
     const divEtiquetas = document.createElement("div");
-    divEtiquetas.className = "gasto-etiquetas";
+    //divEtiquetas.className = "gasto-etiquetas";
+    divEtiquetas.classList.add("gasto-etiquetas");
 
     // Si hay etiquetas, las añadimos
     gasto.etiquetas.forEach(etiqueta => {
         const cadaEtiqueta = document.createElement("span");
-        cadaEtiqueta.className = "gasto-etiquetas-etiqueta";
-        cadaEtiqueta.textContent = etiqueta;
-        divEtiquetas.appendChild(cadaEtiqueta);
+        //cadaEtiqueta.className = "gasto-etiquetas-etiqueta";
+            cadaEtiqueta.classList.add("gasto-etiquetas-etiqueta");
+            cadaEtiqueta.textContent = etiqueta;
+            divEtiquetas.appendChild(cadaEtiqueta);
         });
 
-    return
+    //Al final: añadir todo
+
+    // Añadir todos los elementos al div principal
+    divGranGasto.appendChild(divDescripcion);
+    divGranGasto.appendChild(divFecha);
+    divGranGasto.appendChild(divValor);
+    divGranGasto.appendChild(divEtiquetas);
+
+    // Insertar el nuevo elemento en el contenedor
+    contenedor.appendChild(divGranGasto);
 }
 
 function mostrarGastosAgrupadosWeb(idElemento, agrup, periodo){
