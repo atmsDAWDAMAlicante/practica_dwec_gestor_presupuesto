f9b4554b9db168901c5a13d26cc114e8d45363c5
atmsDAWDAMAlicante <angmorsen@alu.edu.gva.es>
2025-01-25 19:18:17 +0100

    actualizarPresupuesto y mostrarDatoEnId dos primeros epígrafes

--- js/generarDatosEstaticos.js
+++ js/generarDatosEstaticos.js
@@ -1,5 +1,11 @@
 import * as gestionPresupuesto from './gestionPresupuesto.js';
 import * as gestionPresupuestoWeb from './gestionPresupuestoWeb.js';
 
-// Actualiza el presupuesto a 1500€
+// 1º Actualizar el presupuesto a 1500€ (función actualizarPresupuesto)
 gestionPresupuesto.actualizarPresupuesto(1500);
+
+// 2º Mostrar el presupuesto en el div#presupuesto (funciones mostrarPresupuesto y mostrarDatoEnId)
+const presupuesto = gestionPresupuesto.mostrarPresupuesto();
+gestionPresupuestoWeb.mostrarDatoEnId('presupuesto', presupuesto);
+
+// 3º Crear los siguientes gastos (función crearGasto): 
